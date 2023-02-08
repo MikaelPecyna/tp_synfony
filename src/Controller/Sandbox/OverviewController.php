1e55d7f8b5762323980d48c17088e3bd8151a0b1
Mikael <mikael.pecyna@gmail.com>
2023-02-08 08:35:40 +0100

    6.6 : debut hello4

--- src/Controller/Sandbox/OverviewController.php
+++ src/Controller/Sandbox/OverviewController.php
@@ -13,9 +13,33 @@ class OverviewController extends AbstractController
     {
         return new Response('<body>Hello World!</body>');
     }
+
     #[Route('/sandbox/overview/hello2', name: 'sandbox_overview_hello2')]
     public function hello2Action(): Response
     {
-        return new Response('<body> Hello World 2 ! </body>');
+        return $this->render('Sandbox/Overview/hello2.html.twig');
     }
+
+    #[Route('/sandbox/overview/hello3', name: 'sandbox_overview_hello3')]
+public function hello3(): Response
+    {
+        $args = array(
+            'prenom' => 'Mikael',
+            'jeux' => ['A Plague Tale : Innocence', 'WoW', "Mass Effect", 'Life is Strange'],
+        );
+        return $this->render('Sandbox/overview/hello3.html.twig', $args);
+
+    }
+
+    #[Route('/sandbox/overview/hello4', name: 'sandbox_overview_hello4')]
+    public function hello4(): Response
+    {
+        $args = array(
+            'prenom' => 'Mikael',
+            'jeux' => ['A Plague Tale : Innocence', 'WoW', "Mass Effect", 'Life is Strange'],
+        );
+        return $this->render('Sandbox/overview/hello4.html.twig', $args);
+
+    }
+
 }
