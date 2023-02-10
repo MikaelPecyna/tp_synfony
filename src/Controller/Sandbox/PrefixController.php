8525c24ec35e2359df1fe6f17b4699d4991a617c
Mikael Pecyna <mikael.pecyna@gmail.com>
2023-02-10 23:47:19 +0100

    3:1 - Changement des routes controller prefix

---
+++ src/Controller/Sandbox/PrefixController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller\Sandbox;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/sandbox/prefix', name: 'sandbox_prefix')]
+class PrefixController extends AbstractController
+{
+    #[Route('', name: '')]
+    public function indexAction(): Response
+    {
+        return new Response('<body>Hello World!</body>');
+    }
+
+    #[Route('/hello2', name: '_hello2')]
+    public function hello2Action(): Response
+    {
+        return $this->render('Sandbox/prefix/hello2.html.twig');
+    }
+
+    #[Route('/hello3', name: '_hello3')]
+public function hello3(): Response
+    {
+        $args = array(
+            'prenom' => 'Mikael',
+            'jeux' => ['A Plague Tale : Innocence', 'WoW', "Mass Effect", 'Life is Strange'],
+        );
+        return $this->render('Sandbox/prefix/hello3.html.twig', $args);
+
+    }
+
+    #[Route('/hello4', name: '_hello4')]
+    public function hello4(): Response
+    {
+        $args = array(
+            'prenom' => 'Mikael',
+            'jeux' => ['A Plague Tale : Innocence', 'WoW', "Mass Effect", 'Life is Strange'],
+        );
+        return $this->render('Sandbox/prefix/hello4.html.twig', $args);
+
+    }
+
+}
