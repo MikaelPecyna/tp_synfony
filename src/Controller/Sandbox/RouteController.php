a891917ea1bcb0d07c63aee33512eb38c08d798d
Mikael Pecyna <mikael.pecyna@gmail.com>
2023-02-13 11:37:43 +0100

    3.7 : Finit test2

--- src/Controller/Sandbox/RouteController.php
+++ src/Controller/Sandbox/RouteController.php
@@ -33,4 +33,56 @@ class RouteController extends AbstractController
         return new Response("<body>Route::withDefault : age = " . $age . "(" . gettype($age) . ")</body>");
     }
 
+    #[Route(
+        '/with-constraint/{age}',
+        name:'_with_constraint', 
+        requirements: ['age' => '0|[1-9]\d*'], 
+        defaults: ['age' => 18],
+    )]
+    public function withConstraintAction($age): Response 
+    {
+        dump($age);
+        return new Response('<body> Route::withConstraint : age = ' . $age . '(' . gettype($age) .")</body>" );
+    }
+
+    #[Route(
+        '/test1/{year}/{month}/{filename}.{ext}',
+        name: '_test1', 
+    )]
+        public function test1Action(int $year, int $month, $filename, $ext): Response
+        {
+            $args = array(
+                'title' => 'Test1',
+                'year' => $year,
+                'month' => $month,
+                'filename' => $filename,
+                'ext' => $ext, 
+            );
+
+            return $this->render('Sandbox/Route/test1.html.twig', $args);
+        }
+        #[Route(
+            '/test2/{year}/{month}/{filename}.{ext}',
+            name: '_test2', 
+            requirements: [
+                'year' => '[1-9]\d{0,3}',
+                'month' => '(0?[1-9])|(1[0-2])',
+                'filename' => '[-a-zA-Z]+', 
+                'ext' => 'png|jpg|jpeg|ppm',
+            ]
+        )]
+            public function test2Action(int $year, int $month, string $filename, string $ext): Response
+            {
+                $args = array(
+                    'title' => 'Test2',
+                    'year' => $year,
+                    'month' => $month,
+                    'filename' => $filename,
+                    'ext' => $ext, 
+                );
+    
+                return $this->render('Sandbox/Route/test1.html.twig', $args);
+            }
+
+
 }
