f08036312694b41bb61ce0f07cd7fc56e13ee477
Mikael Pecyna <mikael.pecyna@gmail.com>
2023-02-15 20:05:48 +0100

    FIIIIIN 8.5

--- src/Controller/MagasinController.php
+++ src/Controller/MagasinController.php
@@ -13,9 +13,31 @@ class MagasinController extends AbstractController
     #[Route(
         '/valeur-stock/{id}', 
         name: '_valeur_stock')]
-    public function valeurStockAction(int $id): Response
+    public function valeurStockAction($id): Response
     {
         dump($id);
         return $this->render('Magasin/Valeur_stock.html.twig', ['value' => 154323]);
+    }
+
+    #[Route(
+        '/stock/{id}/{valinf}/{valsup}',
+        name : "_stock_by_price",
+        defaults : [
+            'valinf' => 0,
+            'valsup' => -1,
+        ]
+    )]
+    public function stockByPriceAction($id, int $valinf, int $valsup)
+    {
+        $args = array(
+            'produits' => array(
+                ['nom' => 'RAM',     'prix' => '43', "quantité" => 32],
+                ['nom' => 'souris',  'prix' => '85', "quantité" => 24],
+                ['nom' => 'clavier', 'prix' => '14', "quantité" => 5 ],
+            ),
+        );
+
+        return $this->render("Magasin/Stock.html.twig", $args);
+
     }
 }
