90d7e3b812003faf979f916e760aa73b2cdf8cad
Mikael <mikael.pecyna@gmail.com>
2023-02-22 14:50:31 +0100

    Y'as moyen que ce soit une fin de tp pour moi

--- src/Controller/Sandbox/TwigController.php
+++ src/Controller/Sandbox/TwigController.php
@@ -14,8 +14,29 @@ class TwigController extends AbstractController
         '/vue1', 
         name : '_vue1'
     )]
-    public function index(): Response
+    public function vue1Action(): Response
     {
         return $this->render('Sandbox/Twig/vue1.html.twig');
     }
+
+
+    #[Route(
+        '/vue2', 
+        name : '_vue2'
+    )]
+    public function vue2Action(): Response
+    {
+        return $this->render('Sandbox/Twig/vue2.html.twig');
+    }
+
+    #[Route(
+        '/vue3', 
+        name : '_vue3'
+    )]
+    public function vue3Action(): Response
+    {
+        return $this->render('Sandbox/Twig/vue3.html.twig');
+    }
+
+    
 }
